c81bbbcd8a3140208dade02c71859a00b9feac09
kreopelle <kreopelle@gmail.com>
2018-11-09 16:16:48 -0500

    Build presentational component to render individual sounds

--- client/src/components/sounds/Sound.js
+++ client/src/components/sounds/Sound.js
@@ -1,2 +1,11 @@
 // code to render an individual sound card
 // should render a comments container for comments related to the specific sound
+import React from 'react'
+
+const Sound = ({ sound }) => {
+  return (
+    <li><strong>{sound.title}</strong>: {sound.description}</li>
+  )
+}
+
+export default Sound

ba570b1556633c823cd795dccf3cbc0216cbae2e
kreopelle <kreopelle@gmail.com>
2018-11-08 20:21:40 -0500

    Add button to fetch sounds from Rails API

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      sounds: "",
+    }
+    // gives access to "this" to change state with getSounds()
+    this.getSounds = this.getSounds.bind(this)
+  }
+
+  getSounds() {
+    fetch("/api/sounds")
+    .then(resp => resp.json())
+    .then(sounds => this.setState({
+      // would like to push each record as indiv objects to a sounds array
+      sounds: JSON.stringify(sounds)
+    }
+    ))
+  }
 
   render() {
     return (
       <div className='App'>
-        <h1>Welcome to the App!</h1>
+        <h1>Sounds</h1>
+        <button onClick={this.getSounds}>Get Sounds</button>
+        <p>{this.state.sounds}</p>
       </div>
     );
   }

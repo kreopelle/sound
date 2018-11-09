b8b7e260e9617c25902f1ae38645e6c719d53133
kreopelle <kreopelle@gmail.com>
2018-11-08 21:25:12 -0500

    Create login function

--- client/src/App.js
+++ client/src/App.js
@@ -20,29 +20,58 @@ class App extends Component {
     ))
   }
 
+  login(event){
+    event.preventDefault()
+    const email = document.querySelector("#email").value
+    const password = document.querySelector("#password").value
+    const request = {"auth": {"email": email, "password": password}}
+    console.log(request)
+    fetch("/api/user_token", {
+      method: "POST",
+      data: request,
+      dataType: "json"
+    })
+    .then(resp => resp.json())
+    .then(result => {
+      console.log(result)
+      return localStorage.setItem("jwt", result.jwt)
+      }
+    )
+
+  }
+
   render() {
     return (
       <div className='App'>
         <h1>Sounds</h1>
-        <form>
-          <label htmlFor="email">Email: </label>
-          <br />
-          <input
-            name="email"
-            id="email"
-            type="email"
-          />
-          <br /><br />
-          <label htmlFor="password">Password: </label>
-          <input
-            name="password"
-            id="password"
-            type="password"
-          />
-        </form>
 
-        <button onClick={this.getSounds}>Get Sounds</button>
-        <p>{this.state.sounds}</p>
+        <div className="login">
+          <form onSubmit={this.login}>
+            <label htmlFor="email">Email: </label>
+            <br />
+            <input
+              name="email"
+              id="email"
+              type="email"
+            />
+            <br /><br />
+            <label htmlFor="password">Password: </label>
+            <br />
+            <input
+              name="password"
+              id="password"
+              type="password"
+            />
+            <br /><br />
+            <input type="submit" />
+          </form>
+        </div>
+
+        <div className="sounds">
+          <br />
+          <button onClick={this.getSounds}>Get Sounds</button>
+          <p>{this.state.sounds}</p>
+        </div>
       </div>
     );
   }

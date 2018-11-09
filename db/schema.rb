a3159544ae0b6bfe0882baf80e5e18d98878cef5
kreopelle <kreopelle@gmail.com>
2018-11-08 20:49:05 -0500

    Set default value for admin to false

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_09_013642) do
+ActiveRecord::Schema.define(version: 2018_11_09_014337) do
 
   create_table "sounds", force: :cascade do |t|
     t.string "title"
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 2018_11_09_013642) do
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password_digest"
-    t.boolean "admin"
+    t.boolean "admin", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

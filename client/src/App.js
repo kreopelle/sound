a1cf36e059b496182e6236be9b7c9e369b00c961
kreopelle <kreopelle@gmail.com>
2018-11-09 15:21:40 -0500

    Move sounds functionality to own component

--- client/src/App.js
+++ client/src/App.js
@@ -1,42 +1,14 @@
 import React, { Component } from 'react';
 import LoginContainer from './containers/LoginContainer.js'
+import SoundsContainer from './containers/SoundsContainer.js'
 
 class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      sounds: '',
-    }
-    // gives access to 'this' to change state with getSounds()
-    this.getSounds = this.getSounds.bind(this)
-  }
-
-  getSounds() {
-    fetch('/api/sounds', {
-      method: 'GET',
-      headers: {
-        'Authorization': `Bearer ${localStorage.getItem('jwt')}`
-      },
-    })
-    .then(resp => resp.json())
-    .then(sounds => this.setState({
-      // would like to push each record as indiv objects to a sounds array
-      sounds: JSON.stringify(sounds)
-    }
-    ))
-  }
 
   render() {
     return (
       <div className='App'>
         <LoginContainer />
-
-        <h1>Sounds</h1>
-        <div className='sounds'>
-          <br />
-          <button onClick={this.getSounds}>Get Sounds</button>
-          <p>{this.state.sounds}</p>
-        </div>
+        <SoundsContainer />
       </div>
     );
   }
--- client/src/containers/SoundsContainer.js
+++ client/src/containers/SoundsContainer.js
@@ -1,1 +1,41 @@
-// business logic for displaying sounds 
+// business logic for displaying sounds
+import React, { Component } from 'react'
+
+class SoundsContainer extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      sounds: '',
+    }
+    // gives access to 'this' to change state with getSounds()
+    this.getSounds = this.getSounds.bind(this)
+  }
+
+  getSounds() {
+    fetch('/api/sounds', {
+      method: 'GET',
+      headers: {
+        'Authorization': `Bearer ${localStorage.getItem('jwt')}`
+      },
+    })
+    .then(resp => resp.json())
+    .then(sounds => this.setState({
+      // would like to push each record as indiv objects to a sounds array
+      sounds: JSON.stringify(sounds)
+    }
+    ))
+  }
+
+  render() {
+    return (
+      <div className='sounds'>
+        <h1>Sounds</h1>
+        <button onClick={this.getSounds}>Get Sounds</button>
+        <p>{this.state.sounds}</p>
+      </div>
+    )
+  }
+
+}
+
+export default SoundsContainer

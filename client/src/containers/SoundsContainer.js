3a2e5c1070b6fb133b2021de5f96d37a6e08f621
kreopelle <kreopelle@gmail.com>
2018-11-09 17:16:42 -0500

    Remove React-only fetching code

--- client/src/containers/SoundsContainer.js
+++ client/src/containers/SoundsContainer.js
@@ -10,38 +10,12 @@ class SoundsContainer extends Component {
     this.props.fetchSounds()
   }
 
-  // constructor(props) {
-  //   super(props)
-  //   this.state = {
-  //     sounds: '',
-  //   }
-  //   // gives access to 'this' to change state with getSounds()
-  //   this.getSounds = this.getSounds.bind(this)
-  // }
-
-  // getSounds() {
-  //   fetch('/api/sounds', {
-  //     method: 'GET',
-  //     headers: {
-  //       'Authorization': `Bearer ${localStorage.getItem('jwt')}`
-  //     },
-  //   })
-  //   .then(resp => resp.json())
-  //   .then(sounds => this.setState({
-  //     // would like to push each record as indiv objects to a sounds array
-  //     sounds: JSON.stringify(sounds)
-  //   }
-  //   ))
-  // }
-
   render() {
     return (
       <div className='sounds'>
         <h1>Sounds</h1>
         <CreateSound />
         <Sounds sounds={this.props.sounds} />
-        {/* <button onClick={this.handleOnClick}>Fetch Sounds</button> */}
-        {/* <button onClick={this.getSounds}>Get Sounds</button> */}
       </div>
     )
   }

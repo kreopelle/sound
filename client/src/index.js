a749f4ee20f406c25eec507d5ec9f9127e47cd05
kreopelle <kreopelle@gmail.com>
2018-11-13 18:41:16 -0500

    Move import files to previous location

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk'
-
 import './index.css';
 import App from './App';
 import rootReducer from './reducers/rootReducer'
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 

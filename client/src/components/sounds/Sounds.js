0950c56f3610f32434e4f4e68529311a066a079b
kreopelle <kreopelle@gmail.com>
2018-11-13 18:40:41 -0500

    Remove debugger

--- client/src/components/sounds/Sounds.js
+++ client/src/components/sounds/Sounds.js
@@ -2,11 +2,10 @@
 import React, { Component } from 'react'
 import Sound from './Sound'
 
-class Sounds extends Component { 
+class Sounds extends Component {
   render(){
     const { sounds } = this.props
     const soundList = sounds.map((sound, id) => <Sound key={id} sound={sound} />)
-
     return (
       <ul>
       {soundList}

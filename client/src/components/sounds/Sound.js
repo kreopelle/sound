f969c655480979639bf7d391259d65f8aef75231
kreopelle <kreopelle@gmail.com>
2018-11-14 16:23:42 -0500

    Add Bootstrap Columns

--- client/src/components/sounds/Sound.js
+++ client/src/components/sounds/Sound.js
@@ -1,17 +1,21 @@
 // code to render an individual sound card
 // should render a comments container for comments related to the specific sound
 import React from 'react'
+import { Col, Thumbnail } from 'react-bootstrap'
 
 const Sound = ({ sound }) => {
   return (
-    <li>
-      <p><strong>{sound.title}</strong>: {sound.description}</p>
-      <br />
-      <audio controls>
-        <source src={sound.sound_url} type="audio/mpeg" />
-        Your browser does not support the audio element.
-      </audio>
-    </li>
+    <Col xs={6} md={4}>
+      <Thumbnail>
+        <h3>{sound.title}</h3>
+        <p>{sound.description}</p>
+        <br />
+        <audio controls>
+          <source src={sound.sound_url} type="audio/mpeg" />
+          Your browser does not support the audio element.
+        </audio>
+      </Thumbnail>
+    </Col>
   )
 }
 

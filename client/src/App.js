8925d097b6e28a2f65938685ab819f1e96c3ca60
kreopelle <kreopelle@gmail.com>
2018-11-14 15:29:28 -0500

    Nest routes within Switch statement

--- client/src/App.js
+++ client/src/App.js
@@ -4,16 +4,18 @@ import SoundsContainer from './containers/SoundsContainer.js'
 import CreateSoundContainer from './containers/CreateSoundContainer.js'
 import Navigation from './components/navbar/Navigation.js'
 import Home from './components/navbar/Home.js'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 const App = () => (
   <Router>
     <div>
       <Navigation />
-      <Route exact path="/" component={Home} />
-      <Route exact path="/sounds" component={SoundsContainer} />
-      <Route path="/login" component={LoginContainer} />
-      <Route exact path="/sounds/new" component={CreateSoundContainer} />
+      <Switch>
+        <Route path="/sounds/new" component={CreateSoundContainer} />
+        <Route path="/sounds" component={SoundsContainer} />
+        <Route path="/login" component={LoginContainer} />
+        <Route path="/" component={Home} />
+      </Switch>
     </div>
   </Router>
 )

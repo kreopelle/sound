90ab29e357f66500c1b6217f4b5d4d37359d667c
kreopelle <kreopelle@gmail.com>
2018-11-14 14:49:56 -0500

    Import CreateSoundContainer and create route for new sound

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import LoginContainer from './containers/LoginContainer.js'
 import SoundsContainer from './containers/SoundsContainer.js'
+import CreateSoundContainer from './containers/CreateSoundContainer.js'
 import Navigation from './components/navbar/Navigation.js'
 import Home from './components/navbar/Home.js'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
@@ -10,9 +11,9 @@ const App = () => (
     <div>
       <Navigation />
       <Route exact path="/" component={Home} />
-      <Route path="/sounds" component={SoundsContainer} />
+      <Route exact path="/sounds" component={SoundsContainer} />
       <Route path="/login" component={LoginContainer} />
-      <Route path="/sounds/new" component={CreateSoundContainer} />
+      <Route exact path="/sounds/new" component={CreateSoundContainer} />
     </div>
   </Router>
 )

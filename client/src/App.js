d2e495f2c0b440282c9ae2db9e335e143ac49541
kreopelle <kreopelle@gmail.com>
2018-11-14 14:29:54 -0500

    Move navigation, homepage to own component

--- client/src/App.js
+++ client/src/App.js
@@ -1,44 +1,18 @@
 import React from 'react';
 import LoginContainer from './containers/LoginContainer.js'
 import SoundsContainer from './containers/SoundsContainer.js'
-import { Navbar, NavItem, Nav, NavDropdown, MenuItem } from 'react-bootstrap'
-import { BrowserRouter as Router, Route, Link, NavLink } from 'react-router-dom'
-import { LinkContainer } from 'react-router-bootstrap'
+import Navigation from './components/navbar/Navigation.js'
+import Home from './components/navbar/Home.js'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 const App = () => (
   <Router>
     <div>
-      <Header />
+      <Navigation />
       <Route exact path="/" component={Home} />
       <Route path="/sounds" component={SoundsContainer} />
       <Route path="/login" component={LoginContainer} />
     </div>
   </Router>
 )
-
-const Home = () => <h2>Home</h2>
-
-const Header = () => (
-  <Navbar fluid collapseOnSelect>
-    <Navbar.Header>
-      <Navbar.Brand>
-        <Link to="/">Home</Link>
-      </Navbar.Brand>
-      <Navbar.Toggle />
-    </Navbar.Header>
-    <Navbar.Collapse>
-      <Nav pullRight>
-        <LinkContainer to="/sounds">
-          <NavItem>Sounds</NavItem>
-        </LinkContainer>
-        <LinkContainer to="/login">
-          <NavItem>Login</NavItem>
-        </LinkContainer>
-      </Nav>
-    </Navbar.Collapse>
-  </Navbar>
-);
-
 export default App;
-
-      // <NavLink to="/">Home</NavLink>
---
+++ client/src/components/navbar/Navigation.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Navbar, NavItem, Nav} from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import { LinkContainer } from 'react-router-bootstrap'
+
+const Navigation = () => (
+  <Navbar fluid collapseOnSelect>
+    <Navbar.Header>
+      <Navbar.Brand>
+        <Link to="/">Home</Link>
+      </Navbar.Brand>
+      <Navbar.Toggle />
+    </Navbar.Header>
+    <Navbar.Collapse>
+      <Nav pullRight>
+        <LinkContainer to="/sounds">
+          <NavItem>Sounds</NavItem>
+        </LinkContainer>
+        <LinkContainer to="/login">
+          <NavItem>Login</NavItem>
+        </LinkContainer>
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
+);
+
+export default Navigation

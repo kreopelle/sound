697ce13309dab0674399ebde3fcfee43934eec69
kreopelle <kreopelle@gmail.com>
2018-11-14 10:56:03 -0500

    Add routes to app container

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,34 @@
-import React, { Component } from 'react';
+import React from 'react';
 import LoginContainer from './containers/LoginContainer.js'
 import SoundsContainer from './containers/SoundsContainer.js'
-import NavContainer from './containers/NavBarContainer.js'
 
-class App extends Component {
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
-  render() {
-    return (
-        <NavContainer />
-        <LoginContainer />
-        <SoundsContainer />
-      </div>
-    );
-  }
-}
+const App = () => (
+  <Router>
+    <div>
+      <Header />
+      <Route exact path="/" component={Home} />
+      <Route path="/sounds" component={SoundsContainer} />
+      <Route path="/login" component={LoginContainer} />
+    </div>
+  </Router>
+)
+
+const Home = () => <h2>Home</h2>
+
+const Header = () => (
+  <ul>
+    <li>
+      <Link to="/">Home</Link>
+    </li>
+    <li>
+      <Link to="/sounds">Sounds</Link>
+    </li>
+    <li>
+      <Link to="/login">Login</Link>
+    </li>
+  </ul>
+);
 
 export default App;

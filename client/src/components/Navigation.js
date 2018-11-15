87af96163156fe432ed06cb42a7751f388a7f5dd
kreopelle <kreopelle@gmail.com>
2018-11-15 10:44:28 -0500

    Refactor Navbar to use Bootstrap without react-router-bootstrap LinkContainer

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,29 +1,27 @@
 import React from 'react'
-import { Navbar, NavItem, Nav} from 'react-bootstrap'
-import { Link } from 'react-router-dom'
-import { LinkContainer } from 'react-router-bootstrap'
+import { Navbar, NavItem, Nav } from 'react-bootstrap'
+import { Link, NavLink } from 'react-router-dom'
 
 const Navigation = () => (
   <Navbar fluid collapseOnSelect>
-    <Navbar.Header>
-      <Navbar.Brand>
-        <Link to="/">Home</Link>
-      </Navbar.Brand>
-      <Navbar.Toggle />
-    </Navbar.Header>
-    <Navbar.Collapse>
-      <Nav pullRight>
-        <LinkContainer to="/sounds">
-          <NavItem>Sounds</NavItem>
-        </LinkContainer>
-        <LinkContainer to="sounds/new">
-          <NavItem>New Sound</NavItem>
-        </LinkContainer>
-        <LinkContainer to="/login">
-          <NavItem>Login</NavItem>
-        </LinkContainer>
-      </Nav>
-    </Navbar.Collapse>
+  <Navbar.Header>
+    <Navbar.Brand>
+      <NavLink to="/">Home</NavLink>
+    </Navbar.Brand>
+  </Navbar.Header>
+  <Navbar.Collapse>
+    <Nav>
+      <NavItem>
+        <NavLink to="/sounds">Sounds</NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink to="/sounds/new">New Sound</NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink to="/login">Login</NavLink>
+      </NavItem>
+    </Nav>
+  </Navbar.Collapse>
   </Navbar>
 );
 

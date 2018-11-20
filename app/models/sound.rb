c8911cc7abea957a8fe97220185afe1a4bcf06d5
kreopelle <kreopelle@gmail.com>
2018-11-20 18:18:58 -0500

    Add validations to sound model

--- app/models/sound.rb
+++ app/models/sound.rb
@@ -1,7 +1,17 @@
 class Sound < ApplicationRecord
-  validates :title, :description, presence: true
   has_one_attached :file
 
-  belongs_to :user
+  validates :title, :description, presence: true
+  validate :correct_sound_type
+
+  private
+
+  def correct_sound_type
+   if file.attached? && !file.content_type.audio?
+     errors.add(:file, 'must be audio file')
+   elsif receipt.attached? == false
+     errors.add(:file, 'audio file required')
+   end
+  end
 
 end

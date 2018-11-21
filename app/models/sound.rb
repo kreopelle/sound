dc68123c349c65a62f8b417db1d6c1c21b0e6386
kreopelle <kreopelle@gmail.com>
2018-11-20 19:00:49 -0500

    Remove user from sounds, remove file validation

--- app/models/sound.rb
+++ app/models/sound.rb
@@ -2,16 +2,5 @@ class Sound < ApplicationRecord
   has_one_attached :file
 
   validates :title, :description, presence: true
-  validate :correct_sound_type
-
-  private
-
-  def correct_sound_type
-   if file.attached? && !file.content_type.audio?
-     errors.add(:file, 'must be audio file')
-   elsif receipt.attached? == false
-     errors.add(:file, 'audio file required')
-   end
-  end
 
 end

4656840e976a7956cf7fcb19df21850170fd8f92
kreopelle <kreopelle@gmail.com>
2018-11-20 19:19:58 -0500

    Add custom file validators

--- app/models/sound.rb
+++ app/models/sound.rb
@@ -1,6 +1,8 @@
 class Sound < ApplicationRecord
   has_one_attached :file
 
-  validates :title, :description, presence: true
+  validates :title, :description, presence: true, uniqueness: true 
+  validates :file, attached: true
+  validates :file, content_type: ["audio/mpeg", "audio/mp4", "audio/x-aiff", "audio/ogg"]
 
 end

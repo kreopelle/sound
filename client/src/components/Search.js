61528ae34cd11eede8f8f8a2b89fbabb2e0acab0
kreopelle <kreopelle@gmail.com>
2018-11-21 12:30:50 -0500

    Implement filter for index

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -11,16 +11,16 @@ class Search extends React.Component {
   }
 
   handleChange = event => {
-    this.setState({
-      query: event.target.value
-    })
-    console.log(this.state.query)
+    this.props.onQuery(event)
   }
 
   render(){
     return(
       <Col xs={6} md={4}>
         <Thumbnail>
+          <h1> Sounds </h1>
+          <p> Start typing to filter sounds by title or description </p>
+          <br />
           <form onSubmit={ event => this.props.submit(event) }>
             <input type="text" id="query" placeholder="Filter Sounds" value={this.state.query} onChange={event => this.handleChange(event)} /> <input className="btn btn-info" type="submit" value="Search" />
           </form>
--- client/src/containers/SoundsContainer.js
+++ client/src/containers/SoundsContainer.js
@@ -3,17 +3,42 @@ import { connect } from 'react-redux'
 import Sound from '../components/Sound.js'
 import { deleteSound, favoriteSound } from '../actions/SoundActions.js'
 import Search from '../components/Search.js'
+import { Col, Thumbnail } from 'react-bootstrap'
 
 class SoundsContainer extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      query: '',
+      currentlyDisplayed: this.props.sounds
+    }
+    this.onQuery = this.onQuery.bind(this)
+  }
+
+  onQuery = event => {
+    let filteredSounds = this.props.sounds.filter(sound => sound.title.toLowerCase().includes(event.target.value.toLowerCase()) || sound.description.toLowerCase().includes(event.target.value.toLowerCase()))
+    this.setState({
+      query: event.target.value,
+      currentlyDisplayed: filteredSounds
+    })
+  }
 
   render() {
-    const { sounds, deleteSound, favoriteSound } = this.props
-    const soundList = sounds.map((sound, id) => <Sound key={id} sound={sound} deleteSound={deleteSound} favoriteSound={favoriteSound} />)
+    const { deleteSound, favoriteSound } = this.props
+    const soundList = this.state.currentlyDisplayed.map((sound, id) => <Sound key={id} sound={sound} deleteSound={deleteSound} favoriteSound={favoriteSound} />)
 
     return (
       <div className="container" >
-        <h1 className="text-center">Sounds</h1>
-        <Search />
+        <Col xs={6} md={4}>
+          <Thumbnail>
+            <h1> Sounds </h1>
+            <p> Start typing to filter sounds by title or description </p>
+            <br />
+            <form>
+              <input type="text" id="query" placeholder="Filter Sounds" value={this.state.query} onChange={this.onQuery} /> <input className="btn btn-info" type="submit" value="Search" />
+            </form>
+            </Thumbnail>
+        </Col>
         {soundList}
       </div>
     )
